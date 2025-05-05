bea2ebae32f22dfcb93ea64fe5474af060f66eb2
motasimdev <themotasimdev@gmail.com>
2025-05-06 00:18:33 +0600

    aa

--- js/script.js
+++ js/script.js
@@ -1,1 +1,22 @@
+// function info(name, age) {
+    
+//     console.log("Hello my name is " + name + " my age is " + age);
+    
+// }
+// info("Motasim", 25)
+// info("rafi", 20)
+// info("talid", 23)
 
+// nameless function 
+
+let x = function (){
+    console.log("nameless function");
+    
+}
+x()
+
+let y =()=>{
+    console.log("Arrow function");
+    
+}
+y()
